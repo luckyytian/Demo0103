aad48da2a95ec7ec03341c39ddda4be8f336c59a
luckyytian <luckyytian@163.com>
2017-01-11 14:20:30 +0800

    第一次在as中提交代码

--- app/src/main/java/com/example/a0103demo/MainActivity.java
+++ app/src/main/java/com/example/a0103demo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a0103demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("test", "test git");
     }
 }
